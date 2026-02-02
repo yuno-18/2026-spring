971a6f2e9c2ecc0b1b249e67a313b97232e954c1
yuno-18 <yu.tani18@keio.jp>
2026-02-02 00:08:47 +0000

    ,

--- practice1/02-01/kodon.cpp
+++ practice1/02-01/kodon.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 
 using namespace std;
 
@@ -8,9 +9,31 @@ int main(void){
     cout << "Enter DNA sequence : " ;
     cin >> seq;
 
-    for(int i = 0; i + 3 <= seq.size(); i += 3){
-        cout << seq.substr(i, 3) << " " << endl;
+    for(int i = 0; i < seq.size(); i++){
+        seq[i] = toupper(seq[i]);
     }
+
+    for(int i = 0; i + 3 <= seq.size(); i += 3){
+        cout << seq.substr(i, 3) << " " ;
+    }
+
+    cout << endl;
+
+    for(int i = 0; i + 3 <= seq.size(); i += 3){
+        string codon = seq.substr(i, 3);
+        if(codon == "ATG"){
+            cout << "M";
+        }
+        else if(codon == "TAA" || codon == "TAG"
+                || codon == "TGA" ){
+            break;
+        }
+        else{
+            cout << "?" << " " ;
+        }
+    }
+    
+    cout << endl;
 
 
     return 0;
---
+++ practice1/02-02/Searching_ORF.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main(void){
+    string seq;
+    
+    for(int i = 0; i + 3 <= seq.size(); i += 3){
+        if(seq.substr(i, 3) == "ATG"){
+            
+        }
+    }
+
+
+    return 0;
+}
